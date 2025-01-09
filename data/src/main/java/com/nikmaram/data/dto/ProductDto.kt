403bd696cd76432c505c690291d769f8432dac00
m.hosein <mhnikmaram1@gmail.com>
2025-01-09 16:49:07 +0330

    add data module

---
+++ data/src/main/java/com/nikmaram/data/dto/ProductDto.kt
@@ -0,0 +1,12 @@
+package com.nikmaram.data.dto
+
+
+data class ProductDto(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val image: String,
+)
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Products"
 include(":presentation")
- 
+include(":data")
