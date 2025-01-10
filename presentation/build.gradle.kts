2aa3d5e72df2e0251701ad92e30a54b9650abc72
m.hosein <mhnikmaram1@gmail.com>
2025-01-10 13:45:17 +0330

    add hilt navigation compose

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.hilt.navigation.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
